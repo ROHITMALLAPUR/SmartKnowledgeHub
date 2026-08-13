feffaa08e5cc537700557af5b91ae1c389969b74
Rohit <rohitmallapur612@gmail.com>
2026-08-14 00:19:57 +0530

    Add Document Model

---
+++ SmartKnowledgeHub.API/Models/Document.cs
@@ -0,0 +1,24 @@
+﻿namespace SmartKnowledgeHub.API.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string FileName { get; set; } = string.Empty;
+
+        public string? FilePath { get; set; }
+
+        public string? Content { get; set; }
+
+        public string? Summary { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+
+    }
+}
