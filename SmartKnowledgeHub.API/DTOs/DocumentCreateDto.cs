67e70fe3961920280dffad10e00ee5191cba9e97
Rohit <rohitmallapur612@gmail.com>
2026-08-21 14:59:03 +0530

    Add Dcoument Create DTO

---
+++ SmartKnowledgeHub.API/DTOs/DocumentCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartKnowledgeHub.API.DTOs
+{
+    public class DocumentCreateDto
+    {
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+
+        public string FileName { get; set; } = string.Empty;
+
+        public string? Content { get; set; }
+
+        public string UploadedBy { get; set; } = string.Empty;
+    }
+}
