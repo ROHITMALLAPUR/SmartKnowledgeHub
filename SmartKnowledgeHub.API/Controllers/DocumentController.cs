66cd246f0749cc34671f37b310f9731cd7faaf95
Rohit <rohitmallapur612@gmail.com>
2026-08-21 19:35:40 +0530

    Add Document controller & Inject AppDbContext Object

---
+++ SmartKnowledgeHub.API/Controllers/DocumentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartKnowledgeHub.API.Data;
+
+namespace SmartKnowledgeHub.API.Controllers
+
+{
+    [ApiController]
+    [Route("api/[Contoller]")]
+    public class DocumentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DocumentController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+    }
+
+}
