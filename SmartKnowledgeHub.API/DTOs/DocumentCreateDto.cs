de191977a39815dbc8b1b7df0c3d375d02b6f13b
Rohit <rohitmallapur612@gmail.com>
2026-09-01 23:05:59 +0530

    Add basic validation for create and update DTOs

--- SmartKnowledgeHub.API/DTOs/DocumentCreateDto.cs
+++ SmartKnowledgeHub.API/DTOs/DocumentCreateDto.cs
@@ -9,10 +9,15 @@ namespace SmartKnowledgeHub.API.DTOs
         public string Title { get; set; } = string.Empty;
 
 
+        [Required] 
+        [StringLength(200)]
         public string FileName { get; set; } = string.Empty;
 
         public string? Content { get; set; }
 
+        [Required]
+        [MaxLength(100)]
+        [MinLength(2)]
         public string UploadedBy { get; set; } = string.Empty;
     }
 }
