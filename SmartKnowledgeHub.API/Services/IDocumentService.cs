e005ad50e3ba91597b2bca72fc203f422c78e5e6
Rohit <rohitmallapur612@gmail.com>
2026-09-07 23:29:30 +0530

    Add IDocumentServices Interface

---
+++ SmartKnowledgeHub.API/Services/IDocumentService.cs
@@ -0,0 +1,20 @@
+﻿using SmartKnowledgeHub.API.DTOs;
+
+namespace SmartKnowledgeHub.API.Services
+{
+    public interface IDocumentService
+    {
+        Task<List<DocumentResponseDto>> GetDocumentsAsync();
+
+        Task<DocumentResponseDto?> GetDocumentByIdAsync(int id);
+
+        Task<DocumentResponseDto> CreateDocumentAsync(DocumentCreateDto dto);
+
+        Task<DocumentResponseDto?> UpdateDocumentAsync(
+            int id,
+            DocumentUpdateDto dto);
+
+        Task<bool> DeleteDocumentAsync(int id);
+
+    }
+}
