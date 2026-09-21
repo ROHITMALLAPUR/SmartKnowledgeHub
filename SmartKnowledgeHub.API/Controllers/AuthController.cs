9e9ef3525591c4f2acddd6adea3bee07a9fbebd6
Rohit <rohitmallapur612@gmail.com>
2026-09-21 20:14:50 +0530

    Add AuthController

---
+++ SmartKnowledgeHub.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartKnowledgeHub.API.Services;
+using SmartKnowledgeHub.API.DTOs;
+
+namespace SmartKnowledgeHub.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+            if (!result)
+            {
+                return BadRequest("User with the same email already exists.");
+            }
+            return Ok("User registered successfully.");
+        }
+
+    }
+}
