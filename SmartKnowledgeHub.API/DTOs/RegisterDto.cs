66c4785940818bf14ac5d3442c9a5f1667fb5c98
Rohit <rohitmallapur612@gmail.com>
2026-09-21 19:29:15 +0530

    Add User RegisterDto

---
+++ SmartKnowledgeHub.API/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartKnowledgeHub.API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;    
+    }
+}
